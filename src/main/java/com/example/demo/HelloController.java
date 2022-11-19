975a8ef773769a7476284941faf8f2fe25241b49
m-chiranjeevi1 <93828368+m-chiranjeevi1@users.noreply.github.com>
2022-11-19 18:58:58 +0530

    new method

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -4,6 +4,8 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,5 +23,16 @@ public class HelloController {
 		hostAddr ="My IP address is: "+ ip.getHostAddress();
 		System.out.print(hostAddr);
 		return hostAddr;
+	}
+	
+	@GetMapping("/hai/{text")
+	public String hai(@PathVariable String text) {
+		return "Hello"+text;
+	}
+	
+	@GetMapping("/api/{id}")
+	@ResponseBody
+	public String getEmployeesById(@PathVariable String id) {
+	    return "ID: " + id;
 	}
 }
